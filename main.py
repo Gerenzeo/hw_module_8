951860ae1592f3a39833faeac252b30476ba98f6
Gerenzeo <124388411+Gerenzeo@users.noreply.github.com>
2023-04-22 11:16:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,40 @@
 from datetime import datetime, timedelta
+from collections import defaultdict
 
 users = [
     {"name": 'Paul Wrasker', "birthday": datetime(year=1997, month=4, day=21)},
-    {"name": 'Bill Kinton', "birthday": datetime(year=1986, month=4, day=23)},
-    {"name": 'Maria Sushko', "birthday": datetime(year=1999, month=4, day=18)},
-    {"name": 'Max Cain', "birthday": datetime(year=1974, month=4, day=29)},
-    {"name": 'John Week', "birthday": datetime(year=1990, month=4, day=22)},
-    {"name": 'Sarah Connor', "birthday": datetime(year=1957, month=4, day=21)},
-    {"name": 'Julia Mona', "birthday": datetime(year=1963, month=4, day=16)},
-    {"name": 'Natalia Munic', "birthday": datetime(year=1987, month=4, day=27)},
-    {"name": 'Max Scoot', "birthday": datetime(year=1994, month=4, day=1)},
-    {"name": 'Robert Animil', "birthday": datetime(year=1991, month=4, day=28)}
+    {"name": 'Bill Kinton', "birthday": datetime(year=1986, month=4, day=22)},
+    {"name": 'Maria Sushko', "birthday": datetime(year=1999, month=4, day=23)},
+    {"name": 'Max Cain', "birthday": datetime(year=1974, month=4, day=24)},
+    {"name": 'John Week', "birthday": datetime(year=1990, month=4, day=25)},
+    {"name": 'Sarah Connor', "birthday": datetime(year=1957, month=4, day=26)},
+    {"name": 'Julia Mona', "birthday": datetime(year=1963, month=4, day=27)},
+    {"name": 'Natalia Munic', "birthday": datetime(year=1987, month=4, day=28)},
+    {"name": 'Max Scoot', "birthday": datetime(year=1994, month=5, day=29)},
+    {"name": 'Robert Animil', "birthday": datetime(year=1991, month=4, day=30)}
 ]
 
 def get_birthdays_per_week(users):
-    collected_birthdays = {
-        (0, "monday"): [],
-        (1, "tuesday"): [],
-        (2, "wednesday"): [],
-        (3, "thursday"): [],
-        (4, "friday"): [],
-        (5, "saturday"): [],
-        (6, "sunday"): []
-    }
+    Birthdays = defaultdict(list)
     today = datetime.now()
-    day = 0
+    start_date = datetime(year=today.year, month=today.month, day=today.day)
+    end_date = today + timedelta(days=5)
 
-    for _ in range(7):
-        interval = timedelta(days=day)
-        current_day = interval + today
+    if today.weekday() == 0:
+        start_date = start_date - timedelta(days=2)
 
-        for users_list in users:
-            if (users_list['birthday'].day == current_day.day and users_list['birthday'].month == current_day.month):
-                current_weekday = current_day.weekday()
-                day_of_week = list(collected_birthdays.keys())[current_weekday][1]
-                
-                collected_birthdays[current_weekday, day_of_week].append(users_list['name'])
+    for user in users:
+        user_date = user['birthday'].replace(year=today.year)
+        if start_date <= user_date <= end_date:
+            if user_date.weekday() == 5:
+                user_date = user_date + timedelta(days=2)
+            if user_date.weekday() == 6:
+                user_date = user_date + timedelta(days=1)
+            
+            Birthdays[str(user_date.strftime('%A')).lower()].append(user['name'])
 
-        day += 1
+    for date, names in Birthdays.items():
+        print(f'{date.title()}:', ', '.join(name for name in names))
 
-    collected_birthdays[(0, "monday")].extend(collected_birthdays[(5, "saturday")])
-    collected_birthdays[(0, "monday")].extend(collected_birthdays[(6, "sunday")])
-    collected_birthdays[(5, "saturday")].clear()
-    collected_birthdays[(6, "sunday")].clear()
-
-    for key, values in collected_birthdays.items():
-        if collected_birthdays[key] != []:
-            print(f'{key[1].title()}:', ", ".join([value for value in values]))
-    
 if __name__ == '__main__':
     get_birthdays_per_week(users)
